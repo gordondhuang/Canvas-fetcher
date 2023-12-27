b313ef319656f1ee0c7483b1bd38a61f6c66c14d
Gordon Huang <gordondhuang@gmail.com>
2023-12-26 16:11:28 -0800

    get request for course information from canvas

---
+++ webscraper.py
@@ -0,0 +1,25 @@
+import time
+import requests
+import json
+
+token = open("canvas-token.txt", "r")
+apiKey = token.readline().strip()
+user_id = token.readline().strip()
+#url ="http://canvas.uw.edu/api/v1/accounts/"+user_id+"/courses?access_token="+apiKey
+url ="http://canvas.uw.edu/api/v1/courses/"
+
+headers = {
+    'Authorization': f'Bearer {apiKey}'
+}
+
+params = {
+    'enrollment_type': 'student',  # Change to your preferred enrollment type
+    'enrollment_state': 'active',  # Change to your preferred enrollment state
+}
+
+response = requests.get(url, params=params, headers=headers)
+if response.status_code == 200:
+    courses = response.json()
+    print(courses)
+else:
+    print('Failed to retrieve courses. Status code', response.status_code)
