220e2e38965b8758c2b05de9776f2d38fecde0c8
Gordon Huang <gordondhuang@gmail.com>
2023-12-26 21:13:08 -0800

    working csv file and json file parser

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 This project was created to aid students in organizing their course information to optimize productivity.
 
 ## Variables
-To function correctly the **canvas-token.txt** must be modified
-The first line should be your Canvas API token key(refer to token generation)
+To function correctly the **canvas-token.txt** must be modified.
+The first line entered into the .txt file should be your Canvas API token key(refer to token generation)
 The second line should be your Canvas User_Id(refer to user_id)
 
 ## Canvas API Token key generation
@@ -16,5 +16,5 @@ The second line should be your Canvas User_Id(refer to user_id)
 2. The id is within the json object
 
 ## Documentation
-[CanvasLMS - REST API and Extensions](https://canvas.instructure.com/doc/api/index.html)
-[UW Canvas Policies](https://itconnect.uw.edu/tools-services-support/teaching-learning/canvas/canvas-policies/)
+- [CanvasLMS - REST API and Extensions](https://canvas.instructure.com/doc/api/index.html)
+- [UW Canvas Policies](https://itconnect.uw.edu/tools-services-support/teaching-learning/canvas/canvas-policies/)
--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,38 @@
 import time
 import requests
 import json
+import csv
+
+# Creates a CSV file with a column view of course information
+def create_csv(fileName):
+    with open(fileName, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        headings = ["Course Name", "Instructor", "Section", "Syllabus Link","Time"]
+        writer.writerow(headings)
+
+# Creates the JSON file to create the CSV file
+def create_json(data): 
+    with open ("courses.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
+# Parses the JSON data to convert to CSV
+def json_parser(data):
+    with open("courses.csv", 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        for course in data:
+            course_data = []
+            favorite = course.get('is_favorite') # Check if this is one of the courses that is listed as a favorite
+            
+            if favorite :
+                course_Name = course.get('course_code')
+                instructor_Name = course.get('teachers')[0].get('display_name')
+                section = course.get('sections')[0].get('name')
+                course_data.append(course_Name)
+                course_data.append(instructor_Name)
+                course_data.append(section)
+                writer.writerow(course_data)
+                print(course_data)
 
 token = open("canvas-token.txt", "r")
 apiKey = token.readline().strip()
@@ -14,11 +46,16 @@ headers = {
 params = {
     'enrollment_type': 'student',  # Change to your preferred enrollment type
     'enrollment_state': 'active',  # Change to your preferred enrollment state
+    'exclude_blueprint_courses': True,
+    'state[]': ['available'],
+    'include[]': ['syllabus_body','term', 'sections','teachers','concluded','favorites','public_description']
 }
 
 response = requests.get(url, params=params, headers=headers)
 if response.status_code == 200:
-    courses = response.json()
-    print(courses)
+    courses_data = response.json()
+    create_json(courses_data)
+    create_csv("courses.csv")
+    parsed_courses = json_parser(courses_data)
 else:
     print('Failed to retrieve courses. Status code', response.status_code)
